b89fda47fcc93e45b484e6b4a2b138e16d20b5aa
Vinh10Ngo <phucvinhngovan@gmail.com>
2025-06-30 14:38:08 +0700

    fix scurity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { constants } from './constant';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://source-code:jN1efr3DcKcg3z3c@cluster0.1r1zsfn.mongodb.net/freelance',
+      `mongodb+srv://${constants.mongoDBName}:${constants.mongoDBPassword}@cluster0.1r1zsfn.mongodb.net/freelance`,
     ),
     UsersModule,
     AuthModule,
--- src/auth/constant.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-  secret: 'Phoenix.Red.09',
-};
