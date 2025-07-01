83e277fa66e6e5e9ed64c736b6fe7a0a555f4e59
Vinh10Ngo <phucvinhngovan@gmail.com>
2025-07-01 10:51:15 +0700

    fix dto register

--- src/auth/dto/register.auth.dto.ts
+++ src/auth/dto/register.auth.dto.ts
@@ -1,4 +1,12 @@
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { Role } from '../../function/enums/roles.enum';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class RegisterDto {
   @IsNotEmpty()
@@ -11,4 +19,21 @@ export class RegisterDto {
   @IsNotEmpty()
   @MinLength(6)
   readonly password: string;
+  @IsOptional()
+  @IsString()
+  phone?: string;
+
+  @IsOptional()
+  @IsString()
+  avatar?: string;
+
+  @IsOptional()
+  @IsEnum(['male', 'female', 'other'])
+  gender?: 'male' | 'female' | 'other';
+
+  @IsOptional()
+  @IsString()
+  dateOfBirth?: string;
+  @IsEnum(Role)
+  role?: Role;
 }
