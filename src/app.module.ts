277c3211e6a1564393e7a9c7c740b686b92f73e3
Vinh10Ngo <phucvinhngovan@gmail.com>
2025-07-05 03:49:20 +0700

    feature: user CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // để dùng ở mọi nơi
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.mongoDBName}:${process.env.mongoDBPassword}@cluster0.1r1zsfn.mongodb.net/freelance`,
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
