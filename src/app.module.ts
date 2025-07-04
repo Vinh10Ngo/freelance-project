0414ef1311fb966e569869e6083350ba7b942abc
Vinh10Ngo <phucvinhngovan@gmail.com>
2025-07-05 03:19:46 +0700

    create resource nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://source-code:jN1efr3DcKcg3z3c@cluster0.1r1zsfn.mongodb.net/freelance',
-    ),
-    UsersModule,
-    AuthModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/interface/jwt-payload.ts
+++
@@ -1,4 +0,0 @@
-export interface JwtPayload {
-  sub: string; // hoặc _id
-  email: string;
-}
