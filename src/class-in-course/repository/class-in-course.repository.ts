242ccc30cf92664c0413e8800093554ff3865e9f
Vinh10Ngo <phucvinhngovan@gmail.com>
2025-07-06 16:57:47 +0700

    fix repository class-in-course

--- src/class-in-course/repository/class-in-course.repository.ts
+++ src/class-in-course/repository/class-in-course.repository.ts
@@ -11,32 +11,33 @@ import { Types } from 'mongoose';
 @Injectable()
 export class ClassInCourseRepository {
   constructor(
-    @InjectModel(ClassInCourse.name) private courseModel: Model<ClassInCourse>,
+    @InjectModel(ClassInCourse.name)
+    private classInCourseModel: Model<ClassInCourse>,
   ) {}
 
   async create(data: Partial<ClassInCourse>) {
-    return this.courseModel.create(data);
+    return this.classInCourseModel.create(data);
   }
 
   async findAll() {
-    return this.courseModel.find().exec();
+    return this.classInCourseModel.find().exec();
   }
 
   async findById(id: Types.ObjectId): Promise<ClassInCourseDocument | null> {
-    return this.courseModel.findOne({ id }).exec();
+    return this.classInCourseModel.findOne({ id }).exec();
   }
 
   async findByOneEmail(email: string): Promise<ClassInCourseDocument | null> {
-    return this.courseModel.findOne({ email: email }).exec();
+    return this.classInCourseModel.findOne({ email: email }).exec();
   }
 
   async updateOne(
     id: Types.ObjectId,
     body: object,
   ): Promise<UpdateResult | null> {
-    return this.courseModel.updateOne({ _id: id }, body).exec();
+    return this.classInCourseModel.updateOne({ _id: id }, body).exec();
   }
   async deleleOne(id: Types.ObjectId) {
-    return this.courseModel.updateOne({ id }).exec();
+    return this.classInCourseModel.updateOne({ id }).exec();
   }
 }
